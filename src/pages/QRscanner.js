3feaa98b62e06a77e293701e5ab996f037781dfd
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-11-11 16:59:54 +0100

    fixed rescan functionality

--- src/pages/QRscanner.js
+++ src/pages/QRscanner.js
@@ -4,18 +4,19 @@ import Papa from 'papaparse';
 
 function QRscanner() {
   // const users = ['LordRanchoatos', 'Emma', 'Rexy', 'Fiyin'];
-  const [ users, setUsers ] =useState([])
+  const [users, setUsers] = useState([]);
   const [data, setData] = useState('No result');
   const [verified, setVerified] = useState(false);
+  const [delayScan, setDelayScan] = useState(500);
 
   useEffect(() => {
-    const isReg = users.includes(data)
-    console.log(typeof data, data)
-    console.log(typeof users[0])
+    const isReg = users.includes(data);
+    console.log(typeof data, data);
+    console.log(typeof users[0]);
     if (isReg) {
-      setVerified(isReg)
+      setVerified(isReg);
     }
-    console.log(verified)
+    console.log(verified);
     // console.log(data);
     // for (let i = 0; i < users.length; i++) {
     //   if (data === users[i]) {
@@ -26,10 +27,10 @@ function QRscanner() {
     //   }
     //   console.log(verified);
     // }
-  }, [data]);
+  }, [data, users, verified]);
 
   // const addr = function(results){
-  //   let list = results.map((item) => 
+  //   let list = results.map((item) =>
   //     item
   //   )
   //   setUsers(list)
@@ -37,24 +38,22 @@ function QRscanner() {
 
   const handleUpload = () => {
     const allTx = [];
-    Papa.parse(document.getElementById('uploadfile').files[0],
-      {
-        download: true,
-        header: true,
-        skipEmptyLines: true,
-        complete: function(results){
-          // console.log(result.data[0],Txhash);
-          for (let i =0; i < results.data.length; i++){
-            allTx.push(results.data[i].Txhash)
-          }
-          setUsers(allTx)
-          // setUsers(results)
-          // addr(results)
+    Papa.parse(document.getElementById('uploadfile').files[0], {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        // console.log(result.data[0],Txhash);
+        for (let i = 0; i < results.data.length; i++) {
+          allTx.push(results.data[i].Txhash);
         }
-      }
-    )
-    console.log(users)
-  }
+        setUsers(allTx);
+        // setUsers(results)
+        // addr(results)
+      },
+    });
+    console.log(users);
+  };
 
   // const handleView = () => {
   //   setVerified('true')
@@ -62,16 +61,16 @@ function QRscanner() {
 
   return (
     <div>
-      <input type='file' id='uploadfile' accept='.csv' />
-      <button onClick={handleUpload} id='uploadconfirm'>Upload File</button>
+      <input type="file" id="uploadfile" accept=".csv" />
+      <button onClick={handleUpload} id="uploadconfirm">
+        Upload File
+      </button>
 
-
-
-      {verified && data !== '' ? (
+      {/* {verified ? (
         <div>
           <h1>verified</h1>
         </div>
-      ) : (
+      ) : ( */}
         <div
           style={{
             margin: 'auto',
@@ -81,27 +80,32 @@ function QRscanner() {
           <QrReader
             ViewFinder={function noRefCheck() {}}
             videoId="video"
-            scanDelay={5000}
+            scanDelay={delayScan}
+            constraints={{
+              facingMode: 'environment',
+            }}
             onResult={(result, error) => {
               if (!!result) {
                 setData(result?.text);
+                setDelayScan(false)
               }
 
               if (!!error) {
-                console.info(error);
+                console.info('QrReader', error);
               }
             }}
             // style={{ height: 240+ "px", width: 320 + 'px'}}
           />
         </div>
-      )}
+      {/* )} */}
       <p>{verified ? 'Valid ticket' : 'User Not Found'}</p>
-      <p>{verified}</p>
+      <p>{`${verified}`}</p>
       <p>{data}</p>
       <button
         onClick={() => {
-          setData('');
           setVerified(false);
+          setDelayScan(500)
+          setData('No result');
         }}
       >
         Scan
